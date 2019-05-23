cd3b6c70b25a94e8a2136acb7417b29662db1867
CodingAlwaysMessy <marcus.wallau@googlemail.com>
2019-05-23 06:10:05 +0200

    mom

--- src/com/company/BiggestElementsSolver.java
+++ src/com/company/BiggestElementsSolver.java
@@ -35,96 +35,55 @@ public class BiggestElementsSolver {
     public static ArrayList<Integer> linearBE(ArrayList<Integer> a, int k) {
         // Theta (n)
 
-        if (a.size() < 5) {
-            // return median direkt
-        }
-
-        // Chunked Liste
-        var chunks = splitList(a);
-
-
         return new ArrayList<>();
     }
 
-    public static int splitList(ArrayList<Integer> l) {
+    public static void quickSort(ArrayList<Integer> a, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(a, low, high);
+            quickSort(a, low, pivotIndex - 1);  //sort left of pivot
+            quickSort(a, pivotIndex, high);  //sort right of pivot
+        }
+    }
+
+
+    private static int partition(ArrayList<Integer> a, int low, int high) {
+        int pivot = mom(a.subList(low, high), (high - low + 1) / 2);
+        int i = low - 1;
+        for (int j = low; j < high; ++j) {
+            if (a.get(j) <= pivot) {
+                Collections.swap(a, ++i, j);
+            }
+        }
+        Collections.swap(a, ++i, high);
+        return i;
+    }
+
+    public static int mom(List<Integer> a, int k) {
+
+        if (a.size() <= 5) {
+            return a.get(a.size() / 2);
+        }
+
+        int chunkSize = 5;
+        AtomicInteger counter = new AtomicInteger();
+
+        Collection<List<Integer>> chunks = a.stream()
+                .sorted()
+                .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / chunkSize))
+                .values();
+
 
         ArrayList<Integer> medians = new ArrayList<>();
 
-        for (int i = 0; i < (l.size() / 5) * 5; i += 5) {
+        for (List<Integer> sublist : chunks) {
 
-            Integer[] iRay = new Integer[5];
+            medians.add(sublist.get(sublist.size() / 2));
 
-            iRay[i % 5] = l.get(i);
-            iRay[i % 5 + 1] = l.get(i + 1);
-            iRay[i % 5 + 2] = l.get(i + 2);
-            iRay[i % 5 + 3] = l.get(i + 3);
-            iRay[i % 5 + 4] = l.get(i + 4);
-
-            Arrays.sort(iRay);
-            medians.add(iRay[2]);
         }
 
-
-        return medians.get(medians.size() / 2);
-    }
-
-    public static void quickSort(ArrayList<Integer> a, int low, int high) {
-
-        if (low < high) {
-            int pivot = partition(a, 0, a.size() - 1);
-            quickSort(a, low, pivot - 1);
-            quickSort(a, pivot + 1, high);
-        }
+        return mom(medians, a.size()/10);
 
 
-    }
-
-    public static int partition(ArrayList<Integer> a, int low, int high) {
-
-        int x = a.get(high);
-        int i = low - 1;
-
-        for (int j = low; j < high - 1; j++) {
-            if (a.get(j) <= x) {
-                i = i + 1;
-                Collections.swap(a, i, j);
-            }
-        }
-
-        Collections.swap(a, i + 1, high);
-        return i + 1;
-
-
-    }
-
-    public void selection(ArrayList<Integer> a, int k) {
-
-        if (a.size() < 5) {
-            // sort, return median
-        }
-
-    }
-
-    public static void quickSort(int[] arr, int low, int high) {
-        if (arr.length <= 0) return;
-        if (low >= high) return;
-        int left = low;
-        int right = high;
-
-        int temp = arr[left];    // dig 1: save the value of the reference
-        while (left < right) {
-            while (left < right && arr[right] >= temp) {   // Pit 2: find backwards and forwards An element smaller than the reference, inserted into the reference position pit 1 in the
-                right--;
-            }
-            arr[left] = arr[right];
-            while (left < right && arr[left] <= temp) {    // Pit 3: Find the element larger than the baseline from the way of going, and put it in the pit 2 just dug
-                left++;
-            }
-            arr[right] = arr[left];
-        }
-        arr[left] = temp;    //The reference value is filled into pit 3, ready to divide and recursively fast
-        System.out.println(" Sorting: " + Arrays.toString(arr));
-        quickSort(arr, low, left - 1);
-        quickSort(arr, left + 1, high);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,12 @@ public class Main {
         ArrayList<Integer> ray = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 9, 5, 8, 7, 4, 12, 6, 13, 17, 14, 19, 100, 22, 33));
 
 
-        BiggestElementsSolver.quickSort(ray, 0, ray.size()-1);
+        BiggestElementsSolver.quickSort(ray, 0, ray.size() - 1);
 
         ray.forEach(System.out::println);
 
-
-        BiggestElementsSolver.linearBE(ray, 2);
-        var x = BiggestElementsSolver.splitList(ray);
+        //System.out.println(BiggestElementsSolver.mom(ray, 3));
+        //var x = BiggestElementsSolver.medianOfMedians(ray);
 
 
         System.out.println(BiggestElementsSolver.bruteForceBE(ray, 4));
