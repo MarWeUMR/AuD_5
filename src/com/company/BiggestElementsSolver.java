bef184dcfbc2b54c30190a8d930e8af7bf1dfd4c
codingalwaysmessy <marcus.wallau@googlemail.com>
2019-05-26 16:57:44 +0200

    Kommentare ergänzt

--- src/com/company/BiggestElementsSolver.java
+++ src/com/company/BiggestElementsSolver.java
@@ -8,18 +8,21 @@ import java.util.stream.IntStream;
 public class BiggestElementsSolver {
 
     public static ArrayList<Integer> bruteForceBE(ArrayList<Integer> a, int k) {
-        // Theta (n*k)
 
         Integer[] iRay = new Integer[k];
 
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) { // Äußere Schleife limitiert die Slots
+
             int lastMax = (i == 0) ? Integer.MAX_VALUE : iRay[i - 1];
             int currentMax = 0;
-            for (int j = 0; j < a.size(); j++) {
+
+            for (int j = 0; j < a.size(); j++) { // innere Schleife geht die Liste von anfang bis ende durch
+
                 if (a.get(j) > currentMax & a.get(j) < lastMax) {
                     currentMax = a.get(j);
                 }
             }
+
             iRay[i] = currentMax;
         }
 
@@ -27,44 +30,39 @@ public class BiggestElementsSolver {
     }
 
     public static ArrayList<Integer> improvedBE(ArrayList<Integer> a, int k) {
-        // Theta (n log n)
-        // heap sort
 
+        // Heap Struktur erzeugen
         Queue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
         heap.addAll(a);
 
 
-        ArrayList<Integer> l = new ArrayList<>();
+        ArrayList<Integer> l = new ArrayList<>(); // Liste für größte Elemente
 
-        while (k-- > 0) {
+        while (k-- > 0) { // Größte Elemente extrahieren
             l.add(heap.poll());
         }
 
         return l;
-
-
     }
 
     public static ArrayList<Integer> linearBE(ArrayList<Integer> a, int k) {
-        // Theta (n)
-        int j = a.size() - k;
 
-        int x = medianOfMedians(a, j);
+        int j = a.size() - k; // Rang konvertieren
+        int x = medianOfMedians(a, j); // O(n) Median von rang j finden
 
-        ArrayList<Integer> l = new ArrayList<>();
+        ArrayList<Integer> l = new ArrayList<>(); // liste für größte elemente
 
-        for (int i = 0; i < a.size(); i++) {
+        for (int i = 0; i < a.size(); i++) { // Elemente filtern (einmal durch die liste)
             if (a.get(i) > x) {
                 l.add(a.get(i));
             }
         }
 
-        ArrayList<Integer> result = new ArrayList<>(l.subList(l.size() - k, l.size()));
+        ArrayList<Integer> result = new ArrayList<>(l.subList(l.size() - k, l.size())); // subliste erzeugen mit größten elementen
 
         Collections.sort(result, Collections.reverseOrder());
 
         return result;
-
     }
 
 
@@ -72,50 +70,40 @@ public class BiggestElementsSolver {
 
         int pivot;
 
-
-        ArrayList<ArrayList<Integer>> chunks = listChunker(a);
-
-        ArrayList<Integer> medians = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> chunks = listChunker(a); // Liste in liste mit length = 5 sublists splitten
+        ArrayList<Integer> medians = new ArrayList<>(); // liste der mediane der sublisten
 
         for (List<Integer> sublist : chunks) {
-
-            medians.add(sublist.get(sublist.size() / 2));
-
+            medians.add(sublist.get(sublist.size() / 2)); // submediane finden
         }
 
-        if (medians.size() <= 5) {
+        if (medians.size() <= 5) { // pivot festlegen oder...
             pivot = medians.get(medians.size() / 2);
-        } else {
+        } else { // rekursiv weitermachen
             pivot = medianOfMedians(medians, (medians.size() - 1) / 2);
         }
 
-        // partitionierung
-
-
+        //Partitionierung
         ArrayList<Integer> low = new ArrayList<>();
         ArrayList<Integer> high = new ArrayList<>();
 
-        for (int j = 0; j < a.size(); j++) {
+        for (int j = 0; j < a.size(); j++) { // Aufteilung in die listen um den median herum
 
-            if (a.get(j) > pivot){
+            if (a.get(j) > pivot) {
                 high.add(a.get(j));
-            } else if (a.get(j) < pivot){
+            } else if (a.get(j) < pivot) {
                 low.add(a.get(j));
             }
-
         }
 
-
-
-
+        // Falls Listengröße noch nicht passend, rekursive schritte um weiter zu filtern
         if (k <= low.size()) {
-            return medianOfMedians((ArrayList) low, k);
+            return medianOfMedians(low, k);
         } else if (k > low.size() + 1) {
-            return medianOfMedians((ArrayList) high, k - low.size() - 1);
+            return medianOfMedians(high, k - low.size() - 1);
         } else {
             return pivot;
         }
-
     }
 
     public static ArrayList<Integer> createSequenceInc(int n) {
@@ -127,9 +115,9 @@ public class BiggestElementsSolver {
     }
 
     private static ArrayList<ArrayList<Integer>> listChunker(List<Integer> l) {
+        // helper methode um die liste in sublisten zu splitten
 
         List<ArrayList<Integer>> newList = new ArrayList<>();
-
 
         int i = 0;
 
@@ -139,22 +127,19 @@ public class BiggestElementsSolver {
 
             for (int j = 0; j < 5; j++) {
 
-                if (j+i < l.size()){
+                if (j + i < l.size()) {
                     chunk.add(l.get(j + i));
                 } else {
                     break;
                 }
-
             }
 
             i += 5;
             Collections.sort(chunk, Collections.reverseOrder());
             newList.add(chunk);
-
         }
 
-
-        return (ArrayList)newList;
+        return (ArrayList) newList;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,6 @@ public class Main {
             for (int order = 0; order < 2; order++) {
 
                 System.out.printf("Order: %s\n", (order == 1) ? "Increasing" : "Decreasing");
-
                 System.out.println("--------------");
 
                 ArrayList<Integer> a = (order == 1)
@@ -30,7 +29,6 @@ public class Main {
                             : (method == 1)
                             ? "Improved"
                             : "Linear");
-
 
                     long t_start_dec = System.nanoTime();
                     if (method == 0) {
@@ -47,14 +45,105 @@ public class Main {
 
                     System.out.printf("Time: %d ns (%3.2e s)\n\n", t, (double) t / 1000000000);
                 }
-
             }
-
-
         }
-
-
-
-
     }
 }
+/*
+========================================
+        Array Size: 1000
+        Order: Decreasing
+        --------------
+        Brute Force
+        Time: 19951667 ns (2.00e-02 s)
+
+        Improved
+        Time: 3216387 ns (3.22e-03 s)
+
+        Linear
+        Time: 3140568 ns (3.14e-03 s)
+
+        Order: Increasing
+        --------------
+        Brute Force
+        Time: 8540581 ns (8.54e-03 s)
+
+        Improved
+        Time: 361044 ns (3.61e-04 s)
+
+        Linear
+        Time: 1450980 ns (1.45e-03 s)
+
+        ========================================
+        Array Size: 10000
+        Order: Decreasing
+        --------------
+        Brute Force
+        Time: 153160776 ns (1.53e-01 s)
+
+        Improved
+        Time: 3311704 ns (3.31e-03 s)
+
+        Linear
+        Time: 6088116 ns (6.09e-03 s)
+
+        Order: Increasing
+        --------------
+        Brute Force
+        Time: 64593773 ns (6.46e-02 s)
+
+        Improved
+        Time: 2459669 ns (2.46e-03 s)
+
+        Linear
+        Time: 7616144 ns (7.62e-03 s)
+
+        ========================================
+        Array Size: 100000
+        Order: Decreasing
+        --------------
+        Brute Force
+        Time: 13257835016 ns (1.33e+01 s)
+
+        Improved
+        Time: 22398273 ns (2.24e-02 s)
+
+        Linear
+        Time: 41544517 ns (4.15e-02 s)
+
+        Order: Increasing
+        --------------
+        Brute Force
+        Time: 11034940946 ns (1.10e+01 s)
+
+        Improved
+        Time: 13717907 ns (1.37e-02 s)
+
+        Linear
+        Time: 12882311 ns (1.29e-02 s)
+
+        ========================================
+        Array Size: 200000
+        Order: Decreasing
+        --------------
+        Brute Force
+        Time: 51594391980 ns (5.16e+01 s)
+
+        Improved
+        Time: 21051318 ns (2.11e-02 s)
+
+        Linear
+        Time: 15409484 ns (1.54e-02 s)
+
+        Order: Increasing
+        --------------
+        Brute Force
+        Time: 44752371856 ns (4.48e+01 s)
+
+        Improved
+        Time: 36224711 ns (3.62e-02 s)
+
+        Linear
+        Time: 15981170 ns (1.60e-02 s)
+
+*/
